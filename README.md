443cbcb75475b29240abea1c6ee48dc7320b446b
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2023-12-05 18:51:52 +0100

    Add divs inside of a container
    The divs will be used for the squares that are changing colors later on.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # odin-etch-a-sketch
+My Etch a Sketch project as part of the Odin Project's curriculum. It showcases various DOM manipulations skills.
---
+++ javascript.js
@@ -0,0 +1,11 @@
+let gridWidth = 16;
+let numberOfSquares = gridWidth * gridWidth;
+
+const container = document.querySelector('.container');
+
+for (i=0; i < numberOfSquares; i++) {
+    let square = document.createElement('div');
+    square.id = 's' + i;
+    square.class = 'square';
+    container.appendChild(square);
+}
