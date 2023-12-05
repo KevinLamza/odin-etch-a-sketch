fe2be5163da88f5fae6e8e59aff8c291e2482ef0
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2023-12-05 20:34:37 +0100

    Move number of squares to javascript.js
    The number of squares will be decided by user
    input, so it needs to be handled in javascript.javascript
    and then further processed as a CSS property instead of being
    hardcoded in styles.css

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
-let gridWidth = 16;
+// define number of squares per side
+let gridWidth = 20;
+
+// calculate total number of squares
 let numberOfSquares = gridWidth * gridWidth;
 
+// give number of squares per side to CSS stylesheet
+let style = document.createElement('style');
+style.innerHTML = `
+:root {
+    --number-squares: ${gridWidth};
+}
+`;
+document.head.appendChild(style);
+
+// create container node for the squares
 const container = document.querySelector('.container');
 
+// create numberOfSquares squares with unique ids
 for (i=0; i < numberOfSquares; i++) {
     let square = document.createElement('div');
     square.id = 's' + i;
